f6bf6e2d316965d1462dcf56627a666d1dcd2e9d
Dumitru Craciun <mediainfo_on@yahoo.com>
2026-04-17 15:11:13 +0100

    Deploy to GitHub Pages via docs folder

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/jammming/',  // Numele repository-ului pe GitHub
+  base: '/jammming/',		// / Numele repository-ului pe GitHub
+  build: {
+    outDir: 'docs',  	// Schimb din 'dist' în 'docs'
+  }
 })
