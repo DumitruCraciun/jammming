2f959751693926fae40c826217d9d26df8f16cea
Dumitru Craciun <mediainfo_on@yahoo.com>
2026-04-20 12:09:30 +0100

    Elimin setarea base pentru Netlify

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/jammming/',		// / Numele repository-ului pe GitHub
+  plugins: [react()],  
   build: {
-    outDir: 'dist',  	// Schimb din 'dist' în 'docs'
+    outDir: 'dist',  	
   }
 })
